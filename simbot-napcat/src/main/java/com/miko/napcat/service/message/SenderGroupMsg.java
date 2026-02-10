7bbb2543f242854b9efcad7e633b53b489c55636
LightRain <3164395730@qq.com>
2026-02-10 18:12:43 +0800

    feat(message): 扩展群消息发送接口功能
    - 添加群组文本消息发送方法 sendGroupText
    - 添加群组回复消息发送方法 sendGroupReply
    - 添加群组图片消息发送方法 sendGroupImage
    - 更新群组@消息发送方法参数类型为 SendGroupAtRequest
    - 完善接口文档注释和参数说明

--- simbot-napcat/src/main/java/com/miko/napcat/service/message/SenderGroupMsg.java
+++ simbot-napcat/src/main/java/com/miko/napcat/service/message/SenderGroupMsg.java
@@ -1,19 +1,40 @@
 package com.miko.napcat.service.message;
 
 
-import com.miko.napcat.service.message.request.SendGroupMsgRequest;
+import com.miko.napcat.service.message.request.SendGroupAtRequest;
+import com.miko.napcat.service.message.request.SendGroupImageRequest;
+import com.miko.napcat.service.message.request.SendGroupReplyRequest;
+import com.miko.napcat.service.message.request.SendGroupTextRequest;
 import com.miko.napcat.service.message.response.SendGroupMsgResponse;
 
 /**
  * 群消息发送接口
- * 提供群组@消息发送功能
  */
 public interface SenderGroupMsg {
+    /**
+     * 发送群组文本消息
+     * @param request 发送群文本消息请求对象，包含目标群号和文本内容等信息
+     * @return SendGroupTextMsgResponse 返回消息发送结果，包括消息ID等信息
+     */
+    SendGroupMsgResponse sendGroupText(SendGroupTextRequest request);
     /**
      * 发送群组@消息
      * @param request 发送群消息请求对象，包含目标群号和消息内容等信息
      * @return SendGroupMsgResponse 返回消息发送结果，包括消息ID等信息
      */
-    SendGroupMsgResponse sendGroupAt(SendGroupMsgRequest request);
+    SendGroupMsgResponse sendGroupAt(SendGroupAtRequest request);
+    /**
+     * 发送群组回复消息
+     * @param request 发送群回复消息请求对象，包含目标群号、回复消息ID和消息内容等信息
+     * @return SendGroupReplyMsgResponse 返回消息发送结果，包括消息ID等信息
+     */
+    SendGroupMsgResponse sendGroupReply(SendGroupReplyRequest request);
+
+    /**
+     * 发送群组图片消息
+     * @param request 发送群图片消息请求对象，包含目标群号和图片内容等信息
+     * @return SendGroupImageMsgResponse 返回消息发送结果，包括消息ID等信息
+     */
+    SendGroupMsgResponse sendGroupImage(SendGroupImageRequest request);
 
 }
