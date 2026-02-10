cf4da07f9dc8450b7553454210e329c0fbfc90dd
LightRain <3164395730@qq.com>
2026-02-10 18:13:01 +0800

    feat(message): 扩展群消息服务支持多种消息类型
    - 添加群组文本消息发送功能，支持SendGroupTextRequest请求
    - 添加群组@消息发送功能，使用SendGroupAtRequest请求对象
    - 添加群组回复消息发送功能，支持SendGroupReplyRequest请求
    - 添加群组图片消息发送功能，支持SendGroupImageRequest请求
    - 为各种消息类型添加详细的JavaDoc注释和参数说明
    - 统一使用NapCatApiService调用相应的API接口

--- simbot-napcat/src/main/java/com/miko/napcat/service/message/SendGroupMsgService.java
+++ simbot-napcat/src/main/java/com/miko/napcat/service/message/SendGroupMsgService.java
@@ -1,6 +1,9 @@
 package com.miko.napcat.service.message;
 
-import com.miko.napcat.service.message.request.SendGroupMsgRequest;
+import com.miko.napcat.service.message.request.SendGroupAtRequest;
+import com.miko.napcat.service.message.request.SendGroupImageRequest;
+import com.miko.napcat.service.message.request.SendGroupReplyRequest;
+import com.miko.napcat.service.message.request.SendGroupTextRequest;
 import com.miko.napcat.service.message.response.SendGroupMsgResponse;
 import com.miko.napcat.enums.NapCatApiEnum;
 import com.miko.napcat.service.NapCatApiService;
@@ -23,6 +26,18 @@ public class SendGroupMsgService implements SenderGroupMsg {
     private final BaseApiService baseApiService;
     private final NapCatApiService napCatApiService;
 
+
+    /**
+     * 发送群组文本消息
+     * @param request 发送群文本消息请求对象，包含目标群号和文本内容等信息
+     * @return SendGroupTextMsgResponse 返回消息发送结果，包括消息ID等信息
+     */
+    @Override
+    public SendGroupMsgResponse sendGroupText(SendGroupTextRequest request) {
+        // 调用NapCat API发送群组文本消息
+        return napCatApiService.callNapCatApi(NapCatApiEnum.SEND_GROUP_MSG, request, SendGroupMsgResponse.class);
+    }
+
     /**
      * 向群组发送@消息
      * 通过调用NapCat API实现群组@功能
@@ -30,8 +45,31 @@ public class SendGroupMsgService implements SenderGroupMsg {
      * @param request 包含群号、@目标等信息的请求对象
      * @return 包含消息发送结果的响应对象
      */
-    public SendGroupMsgResponse sendGroupAt(SendGroupMsgRequest request) {
+    public SendGroupMsgResponse sendGroupAt(SendGroupAtRequest request) {
+        // 调用NapCat API发送群组@消息
         return napCatApiService.callNapCatApi(NapCatApiEnum.SEND_GROUP_AT, request, SendGroupMsgResponse.class);
+    }
+
+    /**
+     * 发送群组回复消息
+     * @param request 发送群回复消息请求对象，包含目标群号和回复内容等信息
+     * @return SendGroupMsgResponse 返回消息发送结果，包括消息ID等信息
+     */
+    @Override
+    public SendGroupMsgResponse sendGroupReply(SendGroupReplyRequest request) {
+        // 调用NapCat API发送群组回复消息
+        return napCatApiService.callNapCatApi(NapCatApiEnum.SEND_GROUP_MSG, request, SendGroupMsgResponse.class);
+    }
+
+    /**
+     * 发送群组图片消息
+     * @param request 发送群图片消息请求对象，包含目标群号和图片信息等
+     * @return SendGroupMsgResponse 返回消息发送结果，包括消息ID等信息
+     */
+    @Override
+    public SendGroupMsgResponse sendGroupImage(SendGroupImageRequest request) {
+        // 调用NapCat API发送群组图片消息
+        return napCatApiService.callNapCatApi(NapCatApiEnum.SEND_GROUP_MSG, request, SendGroupMsgResponse.class);
     }
 
 }
